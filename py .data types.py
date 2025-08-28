8d99731e79ff94af6f0a399e2db368ac358fbabf
sai srujana <srujananagireddy82@gmail.com>
2025-08-28 12:26:05 +0530

    Add files via upload

---
+++ py .data types.py
@@ -0,0 +1,71 @@
+#python data types :
+#python data types tells about the type of the data that variable holding on
+#1)number
+#2)sequence
+#3)boolean
+#4)set
+#5)dictionary 
+#1): number
+#a)integer:_which can represent int()
+#b)float:_ the decimal and fractional values o to n.n to -n.n
+#c)complex:_which complex data type be represented by real and imaginary value"a+bj"
+#program 
+a=123
+print(a)
+print(type(a))
+print(id(a))
+
+a1=-134
+print(a1)
+print(type(a1))
+print(id(a1))
+
+b=23.345
+print(b)
+print(type(b))
+print(id(b))
+
+c=23+56j
+print(c)
+print(type(c))
+print((c))
+
+#3):boolen
+#in this we have two categories
+#1)true
+#2)false
+#program
+a=True
+print(type(a))
+b=False
+print(type(b))
+
+#4):set
+#set is a collection of items a set can be enclosed in flower braces{} and seperated by (,)
+# set syntax :setname={val1,val2,val3,.....}
+#program
+myset={12,34,45,56,89,45+67j,"cse",(12,23,45),"true"}
+print(myset)
+print(type(myset))
+#accesing the elements in a set 
+#print (myset{o})
+
+#5):dictionary
+# A dictionary is a collection of key value pairs
+# it can be represented as {}
+#syntax:mydict={key1,val7,kww2,val2,val3,val4}
+#key is always unique
+#dictionary is mutable
+#program
+mydictionary={"cse","dept"}
+print(mydictionary)
+print(type( mydictionary))
+print (mydictionary)
+
+      
+
+
+
+
+
+
