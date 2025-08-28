9a96733ea8b53ed9934209dc4c2087c41b2b984e
sai srujana <srujananagireddy82@gmail.com>
2025-08-28 22:54:10 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,34 @@
+#variable: variable is a container which hold information.
+#ways to create variable:
+#→1)a variable can also start with a capital letter(a-z)
+#→2)a variable can also start with a small letter(a-z)
+#→3)a variable can also start with underscore(_)
+#ways to not create variabe:
+#→1)a variable should not start with digit (1,2,3)
+#→2)a variable should not start with a reserved keyword(if,else)
+#in python there are 35 keywords
+#good variable:
+#program
+a="hello"
+print(a)
+
+A123=123
+print(A123)
+
+a1='hello'
+print(a1)
+
+abcc="1234"
+print(abcc)
+
+_23=('srujana')
+print(_23)
+
+#bad variables:
+#program
+#123="hello"
+
+#b=ists
+
+#123=123#these are bad variables
+
